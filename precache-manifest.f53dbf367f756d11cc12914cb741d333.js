4fc35246cd564d56a6ea211ab264cd6749fd6c0a
Ben Testani <testaniben@gmail.com>
2021-12-30 15:08:02 -0500

    Updates

---
+++ precache-manifest.f53dbf367f756d11cc12914cb741d333.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a68d4c17cc5d7d35ab02",
+    "url": "/starter_kit/static/css/main.511b50b6.chunk.css"
+  },
+  {
+    "revision": "a68d4c17cc5d7d35ab02",
+    "url": "/starter_kit/static/js/main.a68d4c17.chunk.js"
+  },
+  {
+    "revision": "0bcea10876f6d60cb020",
+    "url": "/starter_kit/static/css/1.6362eba9.chunk.css"
+  },
+  {
+    "revision": "0bcea10876f6d60cb020",
+    "url": "/starter_kit/static/js/1.0bcea108.chunk.js"
+  },
+  {
+    "revision": "f58b143197bb0749ce60",
+    "url": "/starter_kit/static/js/runtime~main.f58b1431.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/starter_kit/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "56594ad645afa1fccb1d53eda0daa45f",
+    "url": "/starter_kit/index.html"
+  }
+];
