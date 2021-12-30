7861c9ff6dd3a266be65e13ebb6e01a56004ac8a
Ben Testani <testaniben@gmail.com>
2021-12-30 14:54:26 -0500

    contract live on ropsten https://ropsten.etherscan.io/address/0x102FBA23041d0b2453DC84645Ad7Bd7990348849

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 require('babel-register');
 require('babel-polyfill');
+const { projectId, mnemonic, etherscanApiKey } = require('./secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   networks: {
@@ -7,6 +9,14 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
   },
   contracts_directory: './src/contracts/',
@@ -24,4 +34,8 @@ module.exports = {
       // }
     }
   },
+  plugins: ['truffle-plugin-verify'],
+  api_keys: {
+    etherscan: etherscanApiKey
+  }
 }
