6a8b90cd076ffad6414474574ff361fc0bc247cd
Ben Testani <testaniben@gmail.com>
2021-12-30 03:49:44 -0500

    base working nft contract + frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,67 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
+import Web3 from 'web3';
 import './App.css';
-
+import Color from '../abis/Color.json';
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: '0x0',
+      balance: '0',
+      contract: null,
+      colors: [],
+    };
+  }
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = Color.networks[networkId];
+    if (networkData) {
+      const abi = Color.abi;
+      const address = networkData.address;
+      const contract = await new web3.eth.Contract(abi, address);
+      this.setState({ contract: contract });
+      const totalSupply = await contract.methods.totalSupply().call();
+      this.setState({ totalSupply: totalSupply });
+      for (let i = 0; i < totalSupply; i++) {
+        const color = await contract.methods.colors(i).call();
+        this.setState({ colors: [...this.state.colors, color] }); // add to colors array
+      }
+    } else {
+      window.alert('Smart contract not deployed to detected network.');
+    }
+  }
+  
+  mint = (color) => {
+    console.log('minting color: ', color);
+    this.state.contract.methods.mint(color).send({ from: this.state.account })
+      .once('receipt', (receipt) => {
+        this.setState({ colors : [...this.state.colors, color] });
+      });
+  }
+
   render() {
     return (
       <div>
@@ -14,37 +73,48 @@ class App extends Component {
           >
             Color NFT
           </a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-white"><span id="account">{this.state.account}</span></small>
+            </li>
+          </ul>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+               <h1>Mint Token</h1>
+                <form onSubmit={(event) =>{
+                  event.preventDefault();
+                  const color = this.color.value
+                  this.mint(color);
+                }}>
+                  <input
+                    type="text"
+                    className='form-control mb-1'
+                    placeholder='Color Hex ex: #ffffff'
+                    ref={(input) => { this.color = input; }} // ref is a react thing that allows us to access the input element 
+                  />
+                  <input type="submit" value="Mint" className="btn btn-primary" />  
+                </form>
               </div>
             </main>
+          </div>
+          <hr/>
+          <div className="row text-center">
+            { this.state.colors.map((color, index) => {
+              return (
+                <div className="col-sm-4" key={index}>
+                  <div className="token" style={{backgroundColor: color}}></div>
+                   <div>{color}</div>
+                  
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
     );
   }
 }
-
 export default App;
