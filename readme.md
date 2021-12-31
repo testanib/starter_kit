1184215cce81032bc0940aef9d1a4fae30d49315
Ben Testani <testaniben@gmail.com>
2021-12-30 19:51:06 -0500

    launched on rinkeby testnet

--- readme.md
+++ readme.md
@@ -2,4 +2,7 @@ Simple Color based Ethereum NFT Smart Contract.  Enter a hex value to mint that 
 
 Contract live on ropsten testnet: https://ropsten.etherscan.io/address/0x102FBA23041d0b2453DC84645Ad7Bd7990348849
 
+Contract live on rinkeby testnet: https://rinkeby.etherscan.io/address/0x2f431883419d63796C28F880E7edbF1296ef4D77
+
+
 Website hosted at: https://testanib.github.io/starter_kit/
--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 require('babel-register');
 require('babel-polyfill');
-const { projectId, mnemonic, etherscanApiKey } = require('./secrets.json');
+const { projectId, mnemonic, etherscanApiKey, rinkebyId } = require('./secrets.json');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
@@ -16,7 +16,15 @@ module.exports = {
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${rinkebyId}`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
     },
   },
   contracts_directory: './src/contracts/',
